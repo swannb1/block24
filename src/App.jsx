a2b997d4f0290a834e40235873f6c742b406fd75
Brandon Swann <brandonjswann@gmail.com>
2024-05-09 14:52:49 -0600

    puppy list

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
+import { useState } from 'react'
+import {puppyList} from './data.js'
 import './App.css'
 
 function App() {
+  const [puppies, setPuppies] = useState(puppyList)
+  console.log("puppyList: ", puppyList);
 
   return (
-      <div></div>
-  )
+      <div className="App">
+        {
+          puppies.map((puppy) => {
+            return <p>{puppy.name}</p>
+           })
+        }
+      </div>
+  );
 }
 
 export default App
